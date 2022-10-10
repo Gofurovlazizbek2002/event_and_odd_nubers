cb2cde8e6c6dd9f2d6c098e73b78b86157e5540b
Gofurovlazizbek2002 <lazizbekgofurov997@gmail.com>
2022-10-10 08:14:18 +0500

    number_even

--- number_even.py
+++ number_even.py
@@ -2,4 +2,23 @@
 
 #Create a variable "var_int" and assign it a four-digit integer value.
 
-#Print the number of even digits in the variable "var_int".
+#Print the number of even digits in the variable "var_int"
+# .#To'rt xonali butun son berilgan. Undagi juft raqamlar sonini toping.
+
+#"var_int" o'zgaruvchisini yarating va unga to'rt xonali butun son qiymatini belgilang.
+
+#"var_int" o'zgaruvchisidagi juft raqamlar sonini chop eting.
+x=1397
+x4 = x % 10
+x = x // 10
+
+x3 = x % 10
+x = x // 10
+
+x2 = x % 10
+x = x // 10
+
+x1 = x % 10
+x = x // 10
+
+print((x1+1)%2 + (x2+1)%2 + (x3+1)%2 + (x4+1)%2)
